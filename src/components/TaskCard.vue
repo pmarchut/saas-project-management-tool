dc6a9273a01dcb799ae0711d191f15bdd31d5d7c
Patryk Marchut <p.marchut@aberit.eu>
2025-02-25 09:42:38 +0100

    Revert "[ADD] TaskCard: added details button"
    This reverts commit de89a6efe2a1820f8a0515395e706ea3f11f8bfb.

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -5,12 +5,11 @@ import {
   CardTitle, 
   CardHeader, 
   Avatar, 
-  CardSubtitle,
-  CardActions 
+  CardSubtitle 
 } from '@progress/kendo-vue-layout';
 import { Chip } from '@progress/kendo-vue-buttons';
-import { Button as KButton } from "@progress/kendo-vue-buttons";
 import { useDateFormat } from '@vueuse/core';
+import { RouterLink } from 'vue-router';
 const props = defineProps<{
   task: Task,
 }>();
@@ -18,42 +17,32 @@ const date = useDateFormat(props.task.dueAt, "MM/DD");
 </script>
 
 <template>
-  <Card>
-    <CardHeader>
-      <div class="flex justify-between">
-        <CardTitle>
-          {{ task.title }}
-        </CardTitle>
-        <Avatar type="image" size="medium" shape="circle">
-          <img
-            style="height: 45px; width: 45px;" 
-            src="https://api.dicebear.com/9.x/adventurer/svg?seed=Ryker"
+  <RouterLink :to="`/boards/${$route.params.id}/task/${task.id}`">
+    <Card>
+      <CardHeader>
+        <div class="flex justify-between">
+          <CardTitle>
+            {{ task.title }}
+          </CardTitle>
+          <Avatar type="image" size="medium" shape="circle">
+            <img
+              style="height: 45px; width: 45px;" 
+              src="https://api.dicebear.com/9.x/adventurer/svg?seed=Ryker"
+            />
+          </Avatar>
+        </div>
+
+        <CardSubtitle>
+          <Chip 
+            v-if="task.dueAt"
+            :text="date"
+            value="chip"
+            rounded="full"
+            icon="k-i-clock"
+            theme-color="info"
           />
-        </Avatar>
-      </div>
-
-      <CardSubtitle>
-        <Chip 
-          v-if="task.dueAt"
-          :text="date"
-          value="chip"
-          rounded="full"
-          icon="k-i-clock"
-          theme-color="info"
-        />
-      </CardSubtitle>
-    </CardHeader>
-
-    <CardActions class="flex justify-end">
-      <KButton
-        :theme-color="'primary'"
-        icon="arrow-right"
-        @click.stop="
-          $router.push(`/boards/${$route.params.id}/task/${task.id}`)
-        "
-      >
-        Task details
-      </KButton>
-    </CardActions>
-  </Card>
+        </CardSubtitle>
+      </CardHeader>
+    </Card>
+  </RouterLink>
 </template>
